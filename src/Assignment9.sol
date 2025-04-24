2f66426e0c115a1927147cc16051edb286169f98
Hawana404 <youssef.hawana@ejust.edu.eg>
2025-04-24 22:23:43 +0200

    ya rab yepush

--- src/Assignment9.sol
+++ src/Assignment9.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 // import Openzeppelin Ownable contract
-import "@openzeppelin/contracts/access/Ownable.sol"; 
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol"; 
 
 // inherit from Ownable contract 
 contract Assignment9 is Ownable {
